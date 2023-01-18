5eda09137dd1b082cf6d2029704e08c03240cad4
Sparrow-benjamin <benjaminmutunga40@gmail.com>
2023-01-18 21:01:41 +0300

    Update biblecreationaccount.py

---
+++ My-python-projects/biblecreationaccount.py
@@ -0,0 +1,28 @@
+print("GOD'S CREATION ACCOUNT\n")
+print("========================\n")
+
+days_acc=int(input("Enter any day here to see what God created each day:\n "))
+if days_acc == 1:
+    print("In the first day God created the heavens and the earth and also light to separate day and night(not sun,moon, or stars)\n")
+elif days_acc == 2:
+    print("In the second day God created vault between the waters to separate water from water.\n")
+elif days_acc == 3:
+    print("In the third day God gathered the water under the sky to one place to separate water and dry land, then commanded the land to produce vegetation of all kind\n")
+elif days_acc == 4:
+    print("In the fourth day God created lights in the vault of the sky to separate the day from the night i.e sun and moon.\n")
+elif days_acc == 5:
+    print("In the fifth day God created the great creatures of the sea and every living thing with which the water teems and that moves about in it, according to their kinds, and every winged bird according to its kind.\n")
+elif days_acc == 6:
+    print("In the sixth day God created mankind and all other living creatures according to their kinds i.e livestock.\n")
+elif days_acc == 7:
+    print("In the seventh day God rested from all His work.\n")
+else:
+    print("Invalid day_acc")
+
+
+
+
+
+
+
+
